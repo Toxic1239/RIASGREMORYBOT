e4681d0088f59b5277b1ee5c41b615cc558c06ec
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-07-27 14:54:49 +0100

    1.3.1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bot = require(__dirname + '/lib/amd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Starting Rias ${VERSION}`)
+    Debug.info(`Starting Rias Gremory ${VERSION}`)
   try {
     await bot.init()
     //bot.logger.info('⏳ Database syncing!')
