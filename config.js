cd68a1c46404b3624f53aac43adba402e11cd60f
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-08-09 22:25:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
-global.api_smd = "https://api-smd.onrender.com";
+global.api_smd = "https://api-smd-1.vercel.app";
 global.scan = "https://riasgremorybot-xcqv.onrender.com/";
 
 global.SESSION_ID =
